c084539e13b289c9d92ce325a0f84015ad7a62c0
Hamza Zouitine <3zouitine@gmail.com>
2018-07-04 18:58:38 +0100

    Init commit

---
+++ src/main/java/com/springspecifications/demo/SpringSpecificationsApplication.java
@@ -0,0 +1,12 @@
+package com.springspecifications.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSpecificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSpecificationsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/springspecifications/demo/SpringSpecificationsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springspecifications.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSpecificationsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
